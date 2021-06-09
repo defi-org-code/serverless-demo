bb0d2fdd6b796bd15abd889ab37360043204ed4a
DanielZlotin <zlotindaniel@gmail.com>
2021-06-09 17:42:49 +0300

    initial

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from zlotin'),
+    body: JSON.stringify('Hello from zlotin 2'),
   };
   return response;
 };
