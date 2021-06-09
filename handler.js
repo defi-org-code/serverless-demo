7b1abaae0954dce8f6c2a6c7ebd51c86bd3015fa
DanielZlotin <zlotindaniel@gmail.com>
2021-06-09 20:08:01 +0300

    serverless

--- handler.js
+++ handler.js
@@ -4,7 +4,7 @@ module.exports.hello = async (event, context) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Hello, the current time is ${new Date().toTimeString()}.`,
+      message: `Hello again, the current time is ${new Date().toTimeString()}.`,
     }),
   };
   return response;
