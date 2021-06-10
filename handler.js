3a45cf6eb1c2187811487edc7e0fcdb7e1a96cf0
Tal Kol <talkol@gmail.com>
2021-06-10 23:26:21 +0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -4,12 +4,12 @@ const fs = require("fs-extra");
 const storage = path.resolve(process.env.HOME_DIR, "storage.json");
 
 async function reader(event, context) {
-  //const param = event.pathParameters.param;
+  const param = event.pathParameters.param;
 
   //const result = await fs.readJson(storage);
 
   //return success({param, timestamp: new Date(result.timestamp).toString()});
-  return success(JSON.stringify(event.pathParameters));
+  return success(JSON.stringify(param));
 }
 
 async function writer(event, context) {
